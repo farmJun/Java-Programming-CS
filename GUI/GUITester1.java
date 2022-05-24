3b70aeeea56f0c628e00fdc3df3a396bb465c971
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-05-24 22:03:46 +0900

    GUITester1
    GUITester1

---
+++ GUI/GUITester1.java
@@ -0,0 +1,15 @@
+package 수업;
+
+import javax.swing.*;
+
+public class GUITester1 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(300, 300);
+		frame.setTitle("My first Frame");
+		frame.setLocation(300, 500);
+		frame.setVisible(true);
+	}
+
+}
